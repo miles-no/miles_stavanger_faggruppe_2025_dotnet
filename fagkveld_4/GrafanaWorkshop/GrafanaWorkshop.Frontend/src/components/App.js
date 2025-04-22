857b06af60bfe467bae61a349f02d188a4019ab3
Ove Jørgensen <ove.kristian.jorgensen@miles.no>
2025-04-22 12:25:45 +0200

    Rename title

--- fagkveld_4/GrafanaWorkshop/GrafanaWorkshop.Frontend/src/components/App.js
+++ fagkveld_4/GrafanaWorkshop/GrafanaWorkshop.Frontend/src/components/App.js
@@ -45,7 +45,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>SignalR Workshop</h1>
+        <h1>Grafana Workshop</h1>
         <textarea
           value={message}
           onChange={(e) => setMessage(e.target.value)}
