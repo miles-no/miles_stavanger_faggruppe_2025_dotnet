e02a9adf3e6962299185d83c3a652c81a28305ba
Ove Jørgensen <ove.kristian.jorgensen@miles.no>
2025-04-22 12:12:09 +0200

    Add initial skeleton

---
+++ fagkveld_4/GrafanaWorkshop/GrafanaWorkshop.Backend/Chat/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace GrafanaWorkshop.Backend.Chat
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await SendMessage("A new user has joined the chat");
+            await base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(string message)
+            => await Clients.All.ReceiveMessage(message);
+    }
+}
